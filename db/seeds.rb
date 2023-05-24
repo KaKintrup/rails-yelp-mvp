5dcd493d78a25e0dd24409579cf7e7730b3cf286
Karen Kintrup <karenkintrup@gmail.com>
2023-05-24 15:13:42 +0200

    current status (uncompleted) of the yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "cleaning the Database"
+Restaurant.destroy_all 
+
+puts 'Creating 5 fake restaurants...'
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Company.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  puts "Restaurant with #{restaurant.id} id was created"
+end
+puts 'Finished!'
